7542cfc282c55ab2490d3223754d699a89a4a668
AnujKaran <128172283+AnujKaran@users.noreply.github.com>
2024-02-18 14:09:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ const cors = require('cors');
 
 const port = process.env.PORT || 7000;
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-const { Server } = require("socket.io");
+// const { Server } = require("socket.io");
 
 
-app.use(cors({ credentials: true, origin: "https://web-socket-sandy.vercel.app" }));
+// app.use(cors({ credentials: true, origin: "https://web-socket-sandy.vercel.app" }));
 
-const io = new Server(server);
-io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-  });
+// const io = new Server(server);
+// io.on('connection', (socket) => {
+//     socket.on('chat message', (msg) => {
+//       io.emit('chat message', msg);
+//     });
+//   });
 
-app.use(express.static('./public'))
+// app.use(express.static('./public'))
 
 app.get('/', async (req,res)=>{
     // res.sendFile('index.html');
     res.status(200).send('hello anuj')
 })
 
-server.listen(port,()=>{
+app.listen(port,()=>{
     console.log('server listening on 7000')
 })
