673333ba2a6e31562f62ce51d3400037927b87ca
Anuj karan <anujkaran42@gmail.com>
2024-02-18 13:56:46 +0530

    helllo everone

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const http = require('http')
+const app = express();
+
+const server = http.createServer(app);
+
+const { Server } = require("socket.io");
+
+
+app.use(cors({ credentials: true, origin: "https://socket1-fzg5.vercel.app" }));
+
+const io = new Server(server);
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+app.use(express.static('./public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile('index.html');
+    // res.send('hello anuj')
+})
+
+server.listen(7000,()=>{
+    console.log('server listening on 7000')
+})
